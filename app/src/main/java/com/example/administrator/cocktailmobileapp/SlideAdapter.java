41e9f48e7647ff072f972cc806b18877ea5a7698
sey2 <casio2978@naver.com>
2022-05-20 22:21:20 +0900

    [Update] 현재온도 알려주는 첫 슬라이드 뷰 추가 (05.20)

--- app/src/main/java/com/example/administrator/cocktailmobileapp/SlideAdapter.java
+++ app/src/main/java/com/example/administrator/cocktailmobileapp/SlideAdapter.java
@@ -18,7 +18,8 @@ public class SlideAdapter extends FragmentPagerAdapter {
 
         mFragmentList.add(
                 SlideFragment.createSlideFragment(
-                        "현재 온도",
+                        "음료수 제조기 전원을 켜주세요!",
+                        "음료수 제조기가 켜져 있어야 온도를 읽을 수 있어요.",
                         R.drawable.image_cocktail_blue,
                         R.color.colorBlue255,
                         R.color.colorOrange255,

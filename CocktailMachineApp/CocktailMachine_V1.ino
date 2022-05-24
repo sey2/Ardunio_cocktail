b4e1ac44830ef9ea527e69dc01d68e4057e75198
sey2 <54762273+sey2@users.noreply.github.com>
2022-05-24 17:24:25 +0900

    [Update] init

--- CocktailMachineApp/CocktailMachine_V1.ino
+++ CocktailMachineApp/CocktailMachine_V1.ino
@@ -12,6 +12,7 @@ DHT dht(DHTPIN, DHTTYPE);
 #define RX 3
 
 SoftwareSerial blueToothSerial(TX, RX);   // 블루투스 객체 선언
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // LCD 객체 선언 SDA A4 SCL A5
 
 int recipes[7][7] = { {20, 0, 0, 20, 20, 0, 0},
                      {20, 20, 0, 0, 20, 0, 0},
@@ -36,9 +37,18 @@ void setup() {
   Serial.println("-- 아두이노 세팅 완료 --");
   int tmp = dht.readTemperature();
   blueToothSerial.print(String(tmp));
+
+   /* Lcd 판 기본설정 */
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Cocktail");
+  
   Serial.print("Temperature: ");  // 이하생략
   Serial.print(tmp);
   Serial.println(" C");
+
+  
 }
 
 void loop() {
@@ -49,7 +59,9 @@ void loop() {
       Serial.print(message);
       Serial.println(" 칵테일을 만듭니다");
       
-      /* 칵테일 만들기 전 LED on */
+      /* 칵테일 만들기 전 LCD on */
+
+      setLCDText("MAKING...");
 
       delay(3000);
       
@@ -60,8 +72,10 @@ void loop() {
         digitalWrite(i + 7, LOW);
       }
   
-      /* 칵테일 만든 후에 LED off */
+      /* 칵테일 만든 후에 LCD off */
       delay(1000);
+
+      setLCDText("COMPLETE!");
     
       /* 
        * 완성메제지 보내기  
@@ -72,4 +86,9 @@ void loop() {
       Serial.print(message);
     }
   }
+}
+
+void setLCDText(String text) {
+    lcd.setCursor(0,1);
+    lcd.print(text);
 }

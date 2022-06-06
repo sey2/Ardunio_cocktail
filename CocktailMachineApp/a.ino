1a7368926b71f5f2c94562bb7dc2db6c3d1d73ec
sey2 <54762273+sey2@users.noreply.github.com>
2022-06-06 18:41:35 +0900

    Update a.ino

--- CocktailMachineApp/a.ino
+++ CocktailMachineApp/a.ino
@@ -58,11 +58,7 @@ void requestEvent() {
 
 
     String a = (String)per1 + "/" + (String)per2 + "/" + (String)per3 + "/" + (String)per4;
-    a.toCharArray(buf, a.length() + 1);
-    for (int i = a.length(); i < 20; i++)
-      buf[i] = 'X';
-    Serial.println(buf);
-    Wire.write(buf);
+    Wire.write(a);
   }
 
 }
